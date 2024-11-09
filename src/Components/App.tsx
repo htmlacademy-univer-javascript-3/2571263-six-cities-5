c734254c235d7d90d0ebd48783270ad5ee1fd931
Daemetry <hrudmirom@gmail.com>
2024-11-09 20:32:10 +0500

    Компоненты

---
+++ src/Components/App.tsx
@@ -0,0 +1,9 @@
+﻿import MainPage from './MainPage.tsx';
+
+type AppProps = {
+  offersCount: number;
+}
+
+export default function App({offersCount}: AppProps) {
+  return <MainPage offersCount={offersCount} />;
+}
